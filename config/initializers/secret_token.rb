a56783686431b7576b3a932fe81134623108f1e5
Amin Ariana <aminka@gmail.com>
2013-11-10 13:58:40 -0800

    Rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DataUriToImgUrl::Application.config.secret_key_base = '8e18d25a943db3491171dc383fb2d98e269880ebeb1c5ae96b85931bfe426b3df0e8b38c1c64d61adaa7985effce12cd9331be88cc5a09b6f5afe02dd5fc300e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DataUriToImgUrl::Application.config.session_store :cookie_store, key: '_data-uri-to-img-url_session'
