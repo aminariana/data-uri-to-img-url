d52c4927e11499c3a4a139757196b797bc21f395
Amin Ariana <aminka@gmail.com>
2013-11-10 14:27:52 -0800

    Add a new action and logic for all actions

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,7 +1,21 @@
 class ImagesController < ApplicationController
   def show
+    @image = Images.find(params[:id])
+  end
+
+  def new
+    @image = Images.new
   end
 
   def create
+    @image = Images.new(params[:image])
+
+    if @image.save
+      format.html { redirect_to @image, notice: 'Image was successfully created.' }
+      format.json { render json: @image, status: :created, location: @image }
+    else
+      format.html { render action: "new" }
+      format.json { render json: @image.errors, status: :unprocessable_entity }
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 DataUriToImgUrl::Application.routes.draw do
-  resources :images, :only => [:show, :create]
+  resources :images, :only => [:show, :new, :create]
 end
 
 #== Route Map
-# Generated on 10 Nov 2013 14:19
+# Generated on 10 Nov 2013 14:27
 #
-# images POST /images(.:format)     images#create
-#  image GET  /images/:id(.:format) images#show
+#    images POST /images(.:format)     images#create
+# new_image GET  /images/new(.:format) images#new
+#     image GET  /images/:id(.:format) images#show
