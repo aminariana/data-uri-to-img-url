7bf4a40a4a63b111554cc6df719f1515575282e2
Amin Ariana <aminka@gmail.com>
2013-11-10 14:39:12 -0800

    Whitelist the right parameter

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -8,14 +8,22 @@ class ImagesController < ApplicationController
   end
 
   def create
-    @image = Image.new(params[:image])
+    @image = Image.new(image_params)
 
-    if @image.save
-      format.html { redirect_to @image, notice: 'Image was successfully created.' }
-      format.json { render json: @image, status: :created, location: @image }
-    else
-      format.html { render action: "new" }
-      format.json { render json: @image.errors, status: :unprocessable_entity }
+    respond_to do |format|
+      if @image.save
+        format.html { redirect_to @image, notice: 'Image was successfully created.' }
+        format.json { render json: @image, status: :created, location: @image }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @image.errors, status: :unprocessable_entity }
+      end
     end
+  end
+
+  private
+
+  def image_params
+    params.require(:image).permit!
   end
 end
