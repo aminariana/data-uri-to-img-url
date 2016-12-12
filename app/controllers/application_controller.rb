a314e600b94c947f79fca781a42e6ae89a9e1704
Amin Ariana <aminka@gmail.com>
2016-12-12 11:12:11 -0800

    Add example.js.html and allow CORS. Fixes #2.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :set_response_headers
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def set_response_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+  end
 end
